41aa9c4f10a5338e6334658b41401ac131f0064d
NAKHIL Amine <56585218+Amine372@users.noreply.github.com>
2024-03-27 10:46:48 +0100

    dbconnect

--- php/db_connect.php
+++ php/db_connect.php
@@ -6,7 +6,6 @@ $password = "";
 $dbname = "cinema";
 
 try {
-    //? Je crée une connexion à ma base de données avec les différents paramètres
     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 } catch (ErrorException $e) {
     echo $e;
